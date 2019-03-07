dc3f51cbd07caeadcb9792e0e2e3d65094b6d537
CosignMyCodesign <dillonwilliamsail@gmail.com>
2019-03-07 13:55:17 -0600

    added a resource type view and template that is being rendered as a block in the index. added minor bootstrap to be able to visualize what I want to do a bit better. Also modularized my views

--- sourcery/urls.py
+++ sourcery/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('login', views.login_user, name='login'),
     path('logout', views.user_logout, name='logout'),
     path('register', views.register, name='register'),
-    path('resourceTypeList', views.resourceTypeList, name='resourceTypeList')
 ]
--- sourcery/views/resourceType_view.py
+++ sourcery/views/resourceType_view.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from ..models import Resource_Type
+from sourcery.models import Resource_Type
 
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def resourceTypeList(request):
 
     all_types = Resource_Type.objects.all()
     context = { 'all_types' : all_types }
-    print("CONTEXT", context)
+    print("all_typesssss", all_types)
     return render(request, 'sourcery/resourceTypeList.html', context)
