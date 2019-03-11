6f5f6599aa9a923aa56a0f36c795135e1693a205
CosignMyCodesign <dillonwilliamsail@gmail.com>
2019-03-11 13:52:56 -0500

    fixed merge conflicts

--- sourcery/views/__init__.py
+++ sourcery/views/__init__.py
@@ -1,3 +1,3 @@
 from .loginRegistration_view import index, login_user, user_logout, register
-from .resourceType_view import resourceTypeList
 from .resource_view import resourceGrid
+from .resourceType_view import resourceTypeList, addResourceType
--- sourcery/views/resourceType_view.py
+++ sourcery/views/resourceType_view.py
@@ -12,3 +12,19 @@ def resourceTypeList(request):
     context = { 'all_types' : all_types }
     print("all_typesssss", all_types)
     return render(request, 'sourcery/resourceTypeList.html', context)
+
+
+# def resourceTypeForm(request):
+#     '''
+#     [Renders the add new resource_type form template]
+#     '''
+#     return render(request, 'workforce/addDepartment.html')
+
+def addResourceType(request):
+
+    resource_type_name = request.POST["resource_type_name"]
+
+    new_department = Department.objects.create(
+        name = resource_type_name
+    )
+    return HttpResponseRedirect(reverse('sourcery:index'))
