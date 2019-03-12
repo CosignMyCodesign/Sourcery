9fc94f87bbbd1ac6fecb565dfcbfd758d1b9740c
CosignMyCodesign <dillonwilliamsail@gmail.com>
2019-03-12 13:40:47 -0500

    now able to add resources and they are posted to the appropriate resource type

--- sourcery/views/resource_view.py
+++ sourcery/views/resource_view.py
@@ -14,20 +14,47 @@ def resourceGrid(request, user_id, resource_type_id):
     print("CONTEEEEEEXXT", context)
     return render(request, 'sourcery/resourceGrid.html', context)
 
+def resourceForm(request):
+    '''
+    [Renders the add new department form template]
+    '''
+    current_user = request.user
+    all_types = Resource_Type.objects.filter(user_id=current_user.id)
+    return render(request, 'sourcery/addResource.html', {'all_types':all_types})
+
 def addResource(request):
-    
-    if request.method == "POST":
-        user = request.user
-        title = request.POST["resource_title"]
-        url = request.POST["resource_url"]
-        image = request.POST["resource_image"]
-        # title = request.POST["resource_type_name"]
+    print("hello")
+    user = request.user
+    title = request.POST["resource_title"]
+    url = request.POST["resource_url"]
+    image = request.POST["resource_image"]
+    resource_type_id = request.POST["type"]
+    print("**************", resource_type_id)
+    resource_type_instance = Resource_Type.objects.get(id=resource_type_id)
 
     new_resource = Resource.objects.create(
         title = title,
         user = user,
         url = url,
         image = image,
+        isCompleted = 0,
+        resource_type = resource_type_instance
+
     )
 
     return HttpResponseRedirect(reverse('sourcery:index'))
+
+    #  if request.method == 'GET':
+    #     payment_form = AddPaymentForm()
+    #     template_name = 'profile/add_payment.html'
+    #     return render(request, template_name, {'payment_form': payment_form})
+
+    
+    # if request.method == "POST":
+    #     customer = request.user.id
+    #     name = request.POST["name"]
+    #     accountNumber = request.POST["accountNumber"]
+
+    # with connection.cursor() as cursor:
+    #     cursor.execute("INSERT into website_paymenttype VALUES(%s, %s, %s, %s, %s)", [None, name, accountNumber, None, customer])
+    #     return HttpResponseRedirect(reverse('website:profile'))
